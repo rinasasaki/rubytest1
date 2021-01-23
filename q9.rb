96885169ae8a5e3eff2368c2899b2d23cae9e04c
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-23 21:58:23 +0900

    Add q9.rb 問題９の回答を作成

---
+++ q9.rb
@@ -0,0 +1,20 @@
+#Dogクラスを作り、「ワン！僕の名前は太郎です。」と出力をするintroductionという名前のクラスメソッドを作成してください。
+#また、その後Dogクラスのインスタンスを作成し、実際にメソッドを呼び出してください。
+
+class Dog
+  
+  def initialize
+    
+  end
+  
+  def  introduction
+    puts "ワン！僕の名前は太郎です。"
+  end
+  
+end
+  
+name = Dog.new
+  
+name.introduction
+
+
