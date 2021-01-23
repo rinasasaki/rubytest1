ce1e17db2d56a3ffe6a561d7be4c2af07161c321
佐々木　莉奈 <1072sasaki@gmail.com>
2021-01-24 00:38:53 +0900

    Add q10.rb 問題１０の回答を作成

---
+++ q10.rb
@@ -0,0 +1,26 @@
+#countriesという配列の中身によって挨拶が変わるgreetingメソッドを、if文を用いて作成してください。
+#countriesには”japan”, “us”, “italy” という文字列を配列にして代入し、
+#greetingメソッドの呼び出し時には greeting(配列の要素) となるように引数を渡して呼び出します。
+#その渡ってきた引数によってそれぞれ、”こんにちは”, “Hello”, “ciao”と出力できるようにしてください。
+#なお、存在しない要素を渡った時は”対応する要素がありません”と出力しましょう。
+
+
+
+
+def greeting(countries)
+  countries = ["japan","us","italy"]
+if 
+  countries == countries[0]
+  puts "こんにちは"
+  elsif 
+  puts countries[1]
+  puts "hello"
+  elsif 
+  puts countries[2]
+  puts "chao"
+    else
+  puts "対応する要素がありません"
+    end
+  end
+
+  puts greeting(2)
